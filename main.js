57cbfb3e78fd260ea7de9c286f489c390630c406
Tyler <tcgerber10567@gmail.com>
2017-02-01 20:31:14 -0500

    beginning of search functionality.

--- main.js
+++ main.js
@@ -10,6 +10,8 @@
 	var characterProfileURL = "/wow/character/";
 	var realmStatusURL = "/wow/realm/status"
 	
+	var searchFields = "&fields=stats,items,guild"
+	
 	window.onload = function(){
 		//setup the function on the button to do stuffs
 		document.querySelector("#search").onclick = Search;
@@ -28,7 +30,7 @@
 		else{
 			fullURL = USBaseURL + realmStatusURL + USLocaleURL + keyURL;
 		}
-		//jquery makes life so much easier. Q.Q
+		//jquery makes life so much easier. Q.Q , also anonymous functions are bae.
 		$.getJSON(fullURL).done(function(data){
 			//clear out the old stuff
 			var selec = document.getElementById("realm");
@@ -44,4 +46,23 @@
 	
 	function Search(){
 		//get which locale to search and build the full URL.
+		var locale = document.getElementById("region").value;
+		var fullURL;
+		//get the character name and realm
+		var characterName = document.querySelector("#characterName").value;
+		var realm = document.querySelector("#realm");
+		if(locale == "EU"){
+			fullURL = EUBaseURL + characterProfileURL + realm + '/' + characterName + GBLocaleURL + searchFields + keyURL;
+		}
+		else{
+			fullURL = USBaseURL + characterProfileURL + realm + '/' + characterName + USLocaleURL + searchFields + keyURL;
+		}
+		//jquery makes life so much easier. Q.Q, also deferred stuff is great. i love it.
+		$.getJSON(fullURL).done(function(data){
+			//use the data somehow
+			console.log(data);
+		}).fail(function(){
+			//let the user know their data was bad. D:
+			document.querySelector("#fail").innerHTML = "Failed to get that character/realm combination. make sure you all of the information is correct and try again.";
+		});
 	}
